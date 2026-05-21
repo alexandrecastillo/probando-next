068a6429d7da4e48144df758b898eda6201026db
alexandrecastillo <acg1097@hotmail.com>
2026-05-20 22:34:43 -0500

    mejoras

--- app/api/mercadopago/route.js
+++ app/api/mercadopago/route.js
@@ -46,7 +46,7 @@ export async function POST(request) {
         },
         back_urls: {
           success: `https://briana-alexandre.my.canva.site/wedding-of-briana-alexandre/gracias`,
-          failure: `${baseUrl}/failure`,
+          failure: `https://briana-alexandre.my.canva.site/wedding-of-briana-alexandre/gracias`,
           pending: `${baseUrl}/pending`,
         },
         auto_return: "approved",
